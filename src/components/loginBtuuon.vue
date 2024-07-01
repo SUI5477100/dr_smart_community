2c6498d93135d8d292e112f7361f849067c72096
朱曼 <14629520+zhuman030803@user.noreply.gitee.com>
2024-07-01 23:07:22 +0800

    feat:修改项目结构

---
+++ src/components/loginBtuuon.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="">
+        <Button type="primary">234</Button>
+    </div>
+</template>
+
+<script>
+// 按需引入Ant Design中的组件
+import Button from 'ant-design-vue/lib/button';
+export default {
+    components: {
+        Button
+    }
+};
+</script>
+
+<style scoped></style>
