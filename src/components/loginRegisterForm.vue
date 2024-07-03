683ee81c8a49fdebca1d0ba226c7e6e5955e1bbe
guaguaaa <11707629+guagua_aa@user.noreply.gitee.com>
2024-07-03 13:58:24 +0800

    feat:登录注册完善

--- src/components/loginRegisterForm.vue
+++ src/components/loginRegisterForm.vue
@@ -6,124 +6,115 @@
         <a-form-item
         :validate-status="userNameError() ? 'error' : ''"
         :help="userNameError() || ''"
-        style="margin-bottom: 10px;"
-        >
-        <a-input
-            v-decorator="[
-            'userName',
-            {
-                rules: [
-                { required: true, message: 'Please input your username!' },
-                ],
-            },
-            ]"
-            placeholder="用户名"
-        >
-            <a-icon
-            slot="prefix"
-            type="user" 
-            style="color: rgba(0, 0, 0, 0.25)"
-            />
-        </a-input>
+        style="margin-bottom: 10px;">
+          <a-input
+              v-decorator="[
+              'userName',
+              {
+                  rules: [
+                  { required: true, message: '请输入手机号' },
+                  ],
+              },]"
+              placeholder="手机号">
+              <a-icon
+              slot="prefix"
+              type="user" 
+              style="color: rgba(0, 0, 0, 0.25)"/>
+          </a-input>
         </a-form-item>
         <a-form-item
         :validate-status="passwordError() ? 'error' : ''"
         :help="passwordError() || ''"
-        style="margin-bottom: 8px;"
-        >
-        <a-input
-            v-decorator="[
-            'password',
-            {
+        style="margin-bottom: 8px;">
+          <a-input
+              v-decorator="[
+              'password',
+              {
                 rules: [
-                { required: true, message: 'Please input your Password!' },
+                { required: true, message: '请输入密码' },
+                { min: 5, message: '长度不少于5个字符'},
+                // { validator: }
                 ],
-            },
-            ]"
-            type="password"
-            placeholder="密码"
-        >
-            <a-icon
-            slot="prefix"
-            type="lock"
-            style="color: rgba(0, 0, 0, 0.25)"
-            />
-        </a-input>
+                trigger:'blur'
+              },
+              ]"
+              type="password"
+              placeholder="密码">
+              <a-icon
+              slot="prefix"
+              type="lock"
+              style="color: rgba(0, 0, 0, 0.25)"/>
+          </a-input>
         </a-form-item>
+        <!-- 注册 -->
         <a-form-item
-        :validate-status="passwordError() ? 'error' : ''"
-        :help="passwordError() || ''"
+        :validate-status="checkPassError() ? 'error' : ''"
+        :help="checkPassError() || ''"
         v-if="isLogin == false"
-        style="margin-bottom: 8px;"
-        >
-        <a-input
-            v-decorator="[
-            'password',
-            {
+        style="margin-bottom: 8px;">
+          <a-input
+              v-decorator="[
+              'checkPass',
+              {
                 rules: [
-                { required: true, message: 'Please input your Password!' },
+                { required: true, message: '请确认密码' },
                 ],
-            },
-            ]"
-            type="password"
-            placeholder="确认密码"
-        >
-            <a-icon
-            slot="prefix"
-            type="lock"
-            style="color: rgba(0, 0, 0, 0.25)"
-            />
-        </a-input>
+                trigger:'blur'
+              },
+              ]"
+              type="password"
+              placeholder="确认密码">
+              <a-icon
+              slot="prefix"
+              type="lock"
+              style="color: rgba(0, 0, 0, 0.25)"/>
+          </a-input>
         </a-form-item>
-        <a-row v-if="isLogin == false">
-            <a-col :span="12">
-                <a-form-item
-                :validate-status="passwordError() ? 'error' : ''"
-                :help="passwordError() || ''"
-                >
+        <a-row v-if="isLogin == false" :gutter="[8,0]">
+            <a-col :span="15" >
+              <a-form-item
+              :validate-status="checkCodeError() ? 'error' : ''"
+              :help="checkCodeError() || ''">
                 <a-input
                     v-decorator="[
-                    'password',
+                    'checkCode',
                     {
-                        rules: [
-                        { required: true, message: 'Please input your Password!' },
-                        ],
+                      rules: [
+                      { required: true, message: '请输入验证码' },
+                      ],
+                      trigger:'blur'
                     },
                     ]"
-                    type="password"
-                    placeholder="手机验证码"
-                >
-                    <a-icon
-                    slot="prefix"
-                    type="lock"
-                    style="color: rgba(0, 0, 0, 0.25)"
-                    />
+                    type="text"
+                    placeholder="验证码">
+                  <a-icon
+                  slot="prefix"
+                  type="number"
+                  style="color: rgba(0, 0, 0, 0.25)"/>
                 </a-input>
-            </a-form-item>
+              </a-form-item>
             </a-col>
-            <a-col :span="10">
+            <a-col :span="6">
                 <a-button
-                type="primary"
                 html-type="submit"
                 :disabled="hasErrors(form.getFieldsError())"
-                style="width: 100%;background-color: #6495ed;"
-                >
+                class="codeButton">
+                <span>发送验证码</span>
                 </a-button>
             </a-col>
         </a-row>
         <a-form-item>
-        <a-button
-            type="primary"
-            html-type="submit"
-            :disabled="hasErrors(form.getFieldsError())"
-            style="width: 100%;background-color: #6495ed;"
-            class="submitButton"
-        >
-        <span v-if="isLogin == true" class="buttonText">登录</span>
-        <span v-else class="buttonText">注册</span>
-        </a-button>
+          <a-button
+              type="primary"
+              html-type="submit"
+              :disabled="hasErrors(form.getFieldsError())"
+              class="submitButton">
+            <span v-if="isLogin == true" class="buttonText">登录</span>
+            <span v-else class="buttonText">注册</span>
+          </a-button>
         </a-form-item>
-        <p style="text-align: right;"><a href="#" @click="changeForm">注册</a>&nbsp;|&nbsp;<a href="#">忘记密码？</a></p>
+        <p v-if="isLogin == true" style="text-align: right;"><a href="#" @click="changeForm">注册</a>&nbsp;|&nbsp;<a href="#">忘记密码？</a></p>
+        <p v-else style="text-align: right;"><a href="#" @click="changeForm">已有账号？去登陆</a></p>
     </a-form>
   </div>
 </template>
@@ -132,12 +123,12 @@ function hasErrors(fieldsError) {
   return Object.keys(fieldsError).some((field) => fieldsError[field]);
 }
 export default {
-    name:"loginRegisterForm",
+  name:"loginRegisterForm",
   data() {
     return {
-        isLogin:true,
+        isLogin:false,
         hasErrors,
-        form: this.$form.createForm(this, { name: "horizontal_login" }),
+        form: this.$form.createForm(this, { name: "horizontal_form" }),
     };
   },
   mounted() {
@@ -157,6 +148,14 @@ export default {
       const { getFieldError, isFieldTouched } = this.form;
       return isFieldTouched("password") && getFieldError("password");
     },
+    checkPassError(){
+      const {getFieldError, isFieldTouched} = this.form;
+      return isFieldTouched("checkPass") && getFieldError("checkPass");
+    },
+    checkCodeError(){
+      const {getFieldError, isFieldTouched } = this.form;
+      return isFieldTouched("checkCode") && getFieldError("checkCode")
+    },
     handleSubmit(e) {
       e.preventDefault();
       this.form.validateFields((err, values) => {
@@ -174,7 +173,7 @@ export default {
 <style scoped>
 .formContainer{
     width: 350px;
-    height: 420px;
+    height: 440px;
     /* background-color: aquamarine; */
     background-color: white;
     display: flex;
@@ -200,7 +199,13 @@ export default {
 .buttonText{
     color: aliceblue;
 }
-.ant-btn  {
-    line-height: 2px; 
+.submitButton{
+  width: 100%;
+  background-color: #6495ed;
+}
+.codeButton {
+  background-color: #ecf5ff;
+  border: 1px solid #6495ed;
+  color: #6495ed;
 }
 </style>
