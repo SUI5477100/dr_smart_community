42eca85e7eecf69a0716ff700c83610e6af0d25a
userName <userEmail>
2024-07-04 13:41:24 +0800

    feat:商品详情页完成

--- src/pages/productDetails.vue
+++ src/pages/productDetails.vue
@@ -12,58 +12,156 @@
       <!-- 商品图片 -->
       <div class="pImgWrapper">
         <a-carousel arrows dots-class="slick-dots slick-thumb">
+
           <a slot="customPaging" slot-scope="props">
             <img :src="getImgUrl(props.i)" />
           </a>
-          <div v-for="(item,index) in 4" :key="index">
-            <img :src="baseUrl + 'abstract0' + item + '.jpg'" />
+          <div v-for="(item,index) in image" :key="index">
+            <img :src="getImgSrc(item)" />
+          </div>
+          <!-- 左右按钮切换图片 -->
+          <div slot="prevArrow" class="custom-slick-arrow" style="left: 10px;zIndex: 1;">
+            <a-icon type="caret-left" />
+          </div>
+          <div slot="nextArrow" class="custom-slick-arrow" style="right: 10px;">
+            <a-icon type="caret-right" />
           </div>
         </a-carousel>
-        <!-- <div class="pImgBig">
-          <img src="../assets/taobao.png" style="width:480px" alt="">
-        </div>
-        <div class="pImgSmall"></div> -->
       </div>
       <!-- 商品详情信息 -->
       <div class="pDetailWrapper">
+        <!-- 右侧商品名称、收藏按钮 -->
         <div class="pDetailHeader">
           <div class="pNameLeft">
             <div class="pName">蒙牛纯牛奶250ml*16</div>
             <div class="pNameSmall">蒙牛纯牛奶250ml*16</div>
           </div>
           <div class="pCollectRight">
-            <a-button type="primary" style="text-align:center">
-              已收藏 <img src="../assets/image/collect.png" style="width:16px" alt="">
+            <a-button type="primary" style="text-align:center;font-size:12px">
+              <span>已收藏</span>
+              <a-icon type="star" />
             </a-button>
           </div>
         </div>
+        <!-- 右侧商品中间价格、销量等 -->
         <ul class="pDetailMiddle">
           <div>
-            <li>门店价格：</li>
-            <li>市场价格：</li>
-            <li>累计销量：</li>
-            <li>收藏次数：</li>
+            <li>
+              <span>门店价格：</span>
+              <span>￥216</span>
+            </li>
+            <li>
+              <span>市场价格：</span>
+              <span>￥0.00</span>
+            </li>
+            <li>
+              <span>累计销量：</span>
+              <span>0件</span>
+            </li>
+            <li>
+              <span>收藏次数：</span>
+              <span>1次</span>
+            </li>
           </div>
         </ul>
-
+        <!-- 右侧底部商品购买地址及数量 -->
+        <div class="pDetailBottom">
+          <!-- 选择自提点部分 -->
+          <div class="selectAddressPickUpSite">
+            <span>选择自提点：</span>
+            <div class="address">
+              <a-select default-value="lucy" style="width: 200px" @change="handleChange">
+                <a-select-option value="jack">
+                  龙湖舜山府
+                </a-select-option>
+                <a-select-option value="lucy">
+                  Lucy
+                </a-select-option>
+                <a-select-option value="disabled" disabled>
+                  Disabled
+                </a-select-option>
+                <a-select-option value="Yiminghe">
+                  yiminghe
+                </a-select-option>
+              </a-select>
+            </div>
+            <div class="pickUpSite">
+              <a-select default-value="lucy" style="width: 200px" @change="handleChange">
+                <a-select-option value="jack">
+                  自提点01
+                </a-select-option>
+                <a-select-option value="lucy">
+                  Lucy
+                </a-select-option>
+                <a-select-option value="disabled" disabled>
+                  Disabled
+                </a-select-option>
+                <a-select-option value="Yiminghe">
+                  yiminghe
+                </a-select-option>
+              </a-select>
+            </div>
+          </div>
+          <!-- 购买数量部分 -->
+          <div class="purchaseQuantity">
+            <span>购买数量:</span>
+            <!-- 选择购买的数量 -->
+            <div class="number">
+              <a-input-number :min="1" :max="100000" :default-value="3" @change="onChange" />
+            </div>
+            <!-- 库存 -->
+            <div class="inventory">
+              <span>库存：</span>
+              <span>999件</span>
+            </div>
+          </div>
+        </div>
+        <!-- 底部加入购物车按钮 -->
+        <div class="addCart">
+          <a-button type="primary" style="text-align:center;font-size:12px">
+            <a-icon type="shopping-cart" />
+            <span>加入购物车</span>
+          </a-button>
+        </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-const baseUrl =
-  'https://raw.githubusercontent.com/vueComponent/ant-design-vue/master/components/vc-slick/assets/img/react-slick/'
+// const baseUrl =
+//   'https://raw.githubusercontent.com/vueComponent/ant-design-vue/master/components/vc-slick/assets/img/react-slick/'
 export default {
   data() {
     return {
-      baseUrl,
+      image: [
+        'local:milk_01.png',
+        // 'https://img11.360buyimg.com/n1/jfs/t1/160370/19/26506/25122/6656d955F587adf33/cbf8f048898edd59.jpg.avif',
+        'https://img11.360buyimg.com/n1/jfs/t1/242644/8/5336/39975/6656d959F636bd15c/b9f5a3662593102a.jpg.avif',
+        'https://img11.360buyimg.com/n1/jfs/t1/217720/34/42141/27861/6656d95aF1fbfd10f/a775be1223956938.jpg.avif',
+        'https://img11.360buyimg.com/n1/jfs/t1/239185/32/9290/27551/6656d95aF44c56931/a401a85c1d83a396.jpg.avif',
+      ],
+      baseUrl: 'https://example.com/images/',
     }
   },
   methods: {
+    getImgSrc(item) {
+      if (item.startsWith('local:')) {
+        return require(`../assets/image/${item.replace('local:', '')}`)
+      }
+      return item
+    },
     getImgUrl(i) {
-      return `${baseUrl}abstract0${i + 1}.jpg`
+      //   return `${baseUrl}abstract0${i + 1}.jpg`
+      return this.getImgSrc(this.image[i])
+    },
+    // 选择自提点 下拉框
+    handleChange(value) {
+      console.log(`selected ${value}`)
+    },
+    // 股买数量的数字输入框
+    onChange(value) {
+      console.log('changed', value)
     },
   },
 }
@@ -91,11 +189,14 @@ export default {
     }
     .pDetailWrapper {
       display: flex;
-      justify-content: space-between;
+      flex-direction: column;
+      //   justify-content: space-between;
       width: 860px;
-      background-color: antiquewhite;
+      //   background-color: antiquewhite;
       //   background-color: antiquewhite;
       .pDetailHeader {
+        display: flex;
+        justify-content: space-between;
         .pNameLeft {
           .pName {
             color: black;
@@ -111,7 +212,109 @@ export default {
         }
       }
       .pDetailMiddle {
-        background-color: aquamarine;
+        display: flex;
+        align-items: center;
+        margin-top: 20px;
+        height: 160px;
+        background-color: #f3f3f3;
+        padding: 20px;
+        div {
+          width: 860px;
+          height: 120px;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+          align-items: flex-start;
+          li:first-child {
+            span:first-child {
+              color: black;
+              font-weight: 500;
+              font-size: 12px;
+            }
+            span:last-child {
+              color: #cb1b29;
+              font-size: 20px;
+              font-weight: 700;
+            }
+          }
+          li:nth-child(2) {
+            span:first-child {
+              color: #bdbdbd;
+              font-weight: 500;
+              font-size: 12px;
+            }
+            span:last-child {
+              text-decoration: line-through;
+              color: black;
+              font-weight: 500;
+            }
+          }
+          li:nth-child(3) {
+            span:first-child {
+              color: black;
+              font-weight: 500;
+              font-size: 12px;
+            }
+            span:last-child {
+              color: black;
+              font-weight: 500;
+            }
+          }
+          li:last-child {
+            span:first-child {
+              color: black;
+              font-weight: 500;
+              font-size: 12px;
+            }
+            span:last-child {
+              color: black;
+              font-weight: 500;
+            }
+          }
+        }
+      }
+      .pDetailBottom {
+        margin-top: 10px;
+        .selectAddressPickUpSite {
+          display: flex;
+          align-items: center;
+          span {
+            color: #969195;
+            font-weight: 400;
+          }
+          .address,
+          .pickUpSite {
+            margin-left: 10px;
+          }
+        }
+        .purchaseQuantity {
+          display: flex;
+          align-items: center;
+          margin-top: 20px;
+          span {
+            color: #969195;
+            font-weight: 400;
+          }
+          .number {
+            margin-left: 35px;
+          }
+          .inventory {
+            margin-left: 10px;
+            span {
+              color: black;
+              font-weight: 400;
+              font-size: 12px;
+            }
+          }
+        }
+      }
+      .addCart {
+        display: flex;
+        align-items: center;
+        margin-top: 20px;
+        span {
+          margin-left: 8px;
+        }
       }
     }
   }
@@ -141,15 +344,44 @@ export default {
   bottom: -100px;
 }
 .ant-carousel ::v-deep .slick-thumb li {
-  width: 100px;
-  height: 100px;
+  width: 75px;
+  height: 75px;
+  margin: 0 5px;
 }
 .ant-carousel ::v-deep .slick-thumb li img {
   width: 100%;
   height: 100%;
-  filter: grayscale(100%);
+  //   filter: grayscale(100%);
 }
 .ant-carousel ::v-deep .slick-thumb li.slick-active img {
   filter: grayscale(0%);
+}
+// 走马灯下方左右箭头样式
+.ant-carousel ::v-deep .slick-slide {
+  //   text-align: center;
+  //   height: 160px;
+  //   line-height: 160px;
+  //   background: #364d79;
+  //   overflow: hidden;
+}
+
+.ant-carousel ::v-deep .custom-slick-arrow {
+  width: 50px;
+  height: 50px;
+  font-size: 50px;
+  color: #f3f3f3;
+  margin-top: 290px;
+  background-color: white;
+  //   opacity: 0.3;
+}
+.ant-carousel ::v-deep .custom-slick-arrow:before {
+  display: none;
+}
+.ant-carousel ::v-deep .custom-slick-arrow:hover {
+  opacity: 0.5;
+}
+
+.ant-carousel ::v-deep .slick-slide h3 {
+  color: #fff;
 }
 </style>
