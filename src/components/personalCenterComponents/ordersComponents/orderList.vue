1398cedbe52978fc743aac6384edb7f2699fe42d
userName <userEmail>
2024-07-08 15:39:19 +0800

    feat:商品列表

--- src/components/personalCenterComponents/ordersComponents/orderList.vue
+++ src/components/personalCenterComponents/ordersComponents/orderList.vue
@@ -1,128 +1,125 @@
 <template>
-    <div class="contain">
-        <div  v-for="goods in goodsList" :key="goods.id">
-            <div class="goods">
-                <div class="goods-item">
-                    <img class="goods-img" :src="goods.url">
-                    <div class="goods-name">{{ goods.name }}</div>
-                </div>
-                <div class="price">
-                    <div>￥</div>
-                    <div class="price-item">{{ goods.price }}</div>
-                </div>
-                <div class="quantity">
-                    <div>x</div>
-                    <div class="quantity-item">{{ goods.quantity }}</div>
-                </div>
-                <div class="subtotal">
-                    <div>￥</div>
-                    <div class="subtotal-item">{{ goods.subtotal }}</div>
-                </div>
-            </div>
-         
+  <div class="contain">
+    <div v-for="goods in goodsList" :key="goods.id">
+      <div class="goods">
+        <div class="goods-item">
+          <img class="goods-img" :src="goods.url">
+          <div class="goods-name">{{ goods.name }}</div>
         </div>
-        <div class="total-price" >
-            <div >总价：</div>
-            <div >272.00</div>
+        <div class="price">
+          <div>￥</div>
+          <div class="price-item">{{ goods.price }}</div>
         </div>
+        <div class="quantity">
+          <div>x</div>
+          <div class="quantity-item">{{ goods.quantity }}</div>
+        </div>
+        <div class="subtotal">
+          <div>￥</div>
+          <div class="subtotal-item">{{ goods.subtotal }}</div>
+        </div>
+      </div>
+
     </div>
+    <div class="total-price">
+      <div>总价：</div>
+      <div>272.00</div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name: 'OrderList', //导出组件名
-    data () {
-        return {
-            goodsList:[{
-                url:require('../../../assets/taobao.png'),
-                name:'强力胶水',
-                price:'34',
-                quantity:'2',
-                subtotal:'68',
-            },
-            {
-                url:require('../../../assets/taobao.png'),
-                name:'强力胶水',
-                price:'34',
-                quantity:'2',
-                subtotal:'68'
-            },
-            {
-                url:require('../../../assets/taobao.png'),
-                name:'强力胶水',
-                price:'34',
-                quantity:'2',
-                subtotal:'68'
-            },
-            {
-                url:require('../../../assets/taobao.png'),
-                name:'强力胶水',
-                price:'34',
-                quantity:'2',
-                subtotal:'68'
-            },
-            
-           ]
-        }
+  name: 'OrderList', //导出组件名
+  data() {
+    return {
+      goodsList: [
+        {
+          url: require('../../../assets/taobao.png'),
+          name: '强力胶水',
+          price: '34',
+          quantity: '2',
+          subtotal: '68',
+        },
+        {
+          url: require('../../../assets/taobao.png'),
+          name: '强力胶水',
+          price: '34',
+          quantity: '2',
+          subtotal: '68',
+        },
+        {
+          url: require('../../../assets/taobao.png'),
+          name: '强力胶水',
+          price: '34',
+          quantity: '2',
+          subtotal: '68',
+        },
+        {
+          url: require('../../../assets/taobao.png'),
+          name: '强力胶水',
+          price: '34',
+          quantity: '2',
+          subtotal: '68',
+        },
+      ],
     }
-};
+  },
+}
 </script>
 
 <style scoped lang="less">
-
-.contain{
-    margin-left: 20px;
-    margin-right: 20px;
+.contain {
+  margin-left: 20px;
+  margin-right: 20px;
 }
 .goods {
-    display: flex;
-  
-    border: 1px solid #f1f1f1;
-    padding: 15px 40px 15px 30px
+  display: flex;
+  border: 1px solid #f1f1f1;
+  padding: 15px 40px 15px 30px;
 }
 
-.goods+.goods {
-    border-top: none;
-    /* 移除除第一个项之外的上边框 */
+.goods + .goods {
+  border-top: none;
+  /* 移除除第一个项之外的上边框 */
 }
 
 .goods-img {
-    width: 50px;
-    height: 50px;
-    background-color: red;
-    margin-right: 15px;
+  width: 50px;
+  height: 50px;
+  background-color: red;
+  margin-right: 15px;
 }
 
 .goods-item {
-    display: flex;
-    flex: 5;
+  display: flex;
+  flex: 5;
 }
 
 .price {
-    display: flex;
-    flex: 2;
+  display: flex;
+  flex: 2;
 }
 
 .quantity {
-    display: flex;
-    flex: 2;
+  display: flex;
+  flex: 2;
 }
-.total-price{
-    display: flex;
-    justify-content: end;
-    color: red;
-    font-size: large;
-    margin-top: 15px;
-    margin-bottom: 25px;
-    font-weight: 500;
-    margin-right: 15px;
+.total-price {
+  display: flex;
+  justify-content: end;
+  color: red;
+  font-size: large;
+  margin-top: 15px;
+  margin-bottom: 25px;
+  font-weight: 500;
+  margin-right: 15px;
 }
 .subtotal {
-    display: flex;
-    color: red;
-    font-size: medium;
-    flex: 1;
-    justify-content: end;
+  display: flex;
+  color: red;
+  font-size: medium;
+  flex: 1;
+  justify-content: end;
 }
 </style>
--- src/pages/orderPayment.vue
+++ src/pages/orderPayment.vue
@@ -21,15 +21,47 @@
       </div> -->
     </div>
     <div class="productMsgWrapper">
-      <div class="tittle"></div>
-      <div class="orderNumber"></div>
+      <div class="tittle">
+        订单商品信息
+      </div>
+      <ul class="orderNumber">
+        <li>订单号：</li>
+        <li>123456789</li>
+      </ul>
+      <div class="msgTittle">
+        <div class="left">商品信息</div>
+        <ul class="right">
+          <li>单价</li>
+          <li>数量</li>
+          <li>小计</li>
+        </ul>
+      </div>
+      <OrderList></OrderList>
+
+    </div>
+    <div class="bottomPay">
+      <span>选择支付方式</span>
+      <div class="btn">
+        <a-button style="width:200px;height:80px;" theme="filled">
+          <a-icon type="pay-circle" theme="filled" style="color:#ffae27;fontSize:24px" />余额支付
+        </a-button>
+        <a-button style="width:200px;height:80px">
+          <a-icon type="wechat" style="color:#22ad38;fontSize:24px" />微信支付
+        </a-button>
+        <a-button style="width:200px;height:80px">
+          <a-icon type="alipay-square" theme="filled" style="color:#00aaed;fontSize:24px" />支付宝
+        </a-button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+// 引入组件
+import OrderList from '../components/personalCenterComponents/ordersComponents/orderList.vue'
 export default {
   name: 'OrderSteps', // 确保组件名是多字词
+  components: { OrderList },
   data() {
     return {
       current: 0,
@@ -65,13 +97,12 @@ export default {
   display: flex;
   flex-direction: column;
   width: 1440px;
-  margin-top: 40px;
-
   margin: auto;
   .stepsWrapper {
     margin-top: 40px;
     width: 800px;
     margin: auto;
+    margin-top: 60px;
     // background-color: pink;
     .steps-content {
       margin-top: 16px;
@@ -85,6 +116,85 @@ export default {
 
     .steps-action {
       margin-top: 24px;
+    }
+  }
+  .productMsgWrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    margin-top: 40px;
+    // background-color: pink;
+    height: 520px;
+    .tittle {
+      color: black;
+      font-size: 16px;
+      font-weight: 500;
+    }
+    .orderNumber {
+      display: flex;
+      padding: 0;
+      margin: 0;
+      li:first-child {
+        color: black;
+        font-size: 12px;
+        font-weight: 400;
+      }
+      li:last-child {
+        color: black;
+        font-size: 12px;
+        font-weight: 400;
+      }
+    }
+    .msgTittle {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      height: 50px;
+      padding: 10px;
+      background-color: #f2f2f2;
+      .left {
+        color: black;
+        font-weight: 400;
+      }
+      .right {
+        width: 800px;
+        display: flex;
+        padding: 0;
+        margin: 0;
+        justify-content: space-around;
+        color: black;
+        font-weight: 400;
+      }
+    }
+  }
+  .bottomPay {
+    span {
+      color: black;
+      font-weight: 500;
+      font-size: 16px;
+    }
+    .btn {
+      width: 700px;
+      margin-top: 16px;
+      display: flex;
+      justify-content: space-between;
+      .ant-btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: black;
+        font-weight: 500;
+        font-size: 16px;
+      }
+      .ant-btn:hover {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: black;
+        font-weight: 500;
+        font-size: 16px;
+        box-shadow: 2px 2px 5px 2px rgba(160, 160, 160, 0.2);
+      }
     }
   }
 }
