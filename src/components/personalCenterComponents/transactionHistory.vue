ea833e841f96321369490cbcb0d654e03b6c2e73
朱曼 <14629520+zhuman030803@user.noreply.gitee.com>
2024-07-08 15:26:29 +0800

    feat:消费记录列表

--- src/components/personalCenterComponents/transactionHistory.vue
+++ src/components/personalCenterComponents/transactionHistory.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        消费记录
+        <a-page-header style="border: 1px solid rgb(235, 237, 240)" title="钱包转账" />
+        <HistoryStatus></HistoryStatus>
+        <HistoryList></HistoryList>
     </div>
 </template>
 
 <script>
-
+import HistoryList from './transactionHistoryComponents/historyList.vue';
+import HistoryStatus from './transactionHistoryComponents/historyStatus.vue';
 export default {
+    components: { HistoryList, HistoryStatus },
     name: 'TransactionHistory', //导出组件名
 };
 </script>
