36b39c9e049cff2c007c85c881b46033cbe5d768
guaguaaa <11707629+guagua_aa@user.noreply.gitee.com>
2024-07-09 13:55:32 +0800

    feat: axios封装，api接口封装统一管理

---
+++ src/api/request.js
@@ -0,0 +1,38 @@
+// 引入axios
+import axios from 'axios';
+import router from '../router';
+
+// 创建网络请求对象,调用axios的create方法
+const requests = axios.create({
+  // 基础路径，发请求的时候，路径当中会出现/api
+  baseURL: '/api',
+  // 超时时间
+  timeout: 5000,
+});
+
+// 请求拦截，发送数据之前
+requests.interceptors.request.use((config) => {
+  config.headers.token = localStorage.getItem('token');
+  return config;
+});
+
+// 响应拦截,获取数据之前
+requests.interceptors.response.use(
+  (res) => {
+    // console.log('request:res.data:' + JSON.stringify(res.data));
+    if (res.data.code === 200) {
+      return res.data;
+    } else if (res.data.code === 401 && res.data.msg === '请先登陆...') {
+      localStorage.setItem('token', '');
+      router.push({ path: '/login' });
+    } else {
+      return res.data;
+    }
+  },
+  (error) => {
+    console.log('error.code:', error.code);
+    console.log(error);
+  }
+);
+
+export default requests;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://smart-community.vnuo.com.cn:9926',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
+        onProxyReq: function (proxyReq, req, res) {
+          console.log('Proxying request:', req.url);
+        },
+      },
+    },
+  },
+});
